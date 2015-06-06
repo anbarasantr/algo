03585e1070ce749ce3f4213fc6b47fc0ed213195
unknown <idmrga@Atlantis-PC.stf.nus.edu.sg>
2015-06-06 19:05:48 +0800

    added list

---
+++ test.py
@@ -0,0 +1,12 @@
+__author__ = 'idmrga'
+from linked_list import Node
+from linked_list import UnorderedList
+
+temp = Node(34)
+print(temp.getData())
+
+l = UnorderedList()
+l.add(5)
+print l.search(5)
+
+
